c8ad853186095e6aaab25fe61fdc35f9e51bd29a
SmolPrzybs <maciek970@gmail.com>
2022-01-21 20:07:10 +0100

    Dodaj pliki projektów.

---
+++ Lab4z2/Lab4z2.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+bool LiczbaPierwsza(int n);
+
+
+int main()
+{
+	int n = 1;
+	for (int i = 0; i < 15; i++)
+	{
+		cout << "liczba = " << n << ", czy pierwsza 0/1 = " << LiczbaPierwsza(n) << endl;
+		n++;
+	}
+
+}
+
+bool LiczbaPierwsza(int n) {
+	if (n < 2) return false;
+
+	for (int i = 2; i * i <= n; i++)
+		if (n % i == 0)
+			return false;
+	return true;
+}
